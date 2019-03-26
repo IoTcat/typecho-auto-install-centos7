dbc152e566771b303967b45fe0f30c4d5ebe6e68
呓喵酱 <i@iotcat.me>
2019-03-26 21:40:09 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -10,28 +10,37 @@ systemctl start nginx
 systemctl enable nginx
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-yum -y install php72w-common
+##yum -y install php72w-common
 wget http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
 rpm -ivh mysql-community-release-el7-5.noarch.rpm
 yum -y install mysql-community-server
 systemctl restart mysqld
-yum -y install php72w-fpm
-systemctl restart php-fpm
-systemctl enable php-fpm
+yum install epel-release
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install yum-utils
+yum-config-manager --enable remi-php72
+yum update
+yum install php72
+yum install php72-php-fpm php72-php-gd php72-php-json php72-php-mbstring php72-php-mysqlnd php72-php-xml php72-php-xmlrpc php72-php-opcache
+systemctl enable php72-php-fpm.service
+systemctl start php72-php-fpm.service
+##yum -y install php72w-fpm
+##systemctl restart php-fpm
+##systemctl enable php-fpm
 cd /etc/nginx
 wget https://yimian-setup.obs.myhwclouds.com/std-conf/nginx.conf -O nginx.conf
 #sed 's/your_domain/$domain/g' nginx.conf
 #yum install -y epel-release
 #yum install -y certbot
 #certbot certonly --webroot -w /opt/www/$domain -d $domain -m $email --agree-tos
-cd /etc/php-fpm.d
-wget https://yimian-setup.obs.myhwclouds.com/std-conf/www.conf -O www.conf
+##cd /etc/php-fpm.d
+##wget https://yimian-setup.obs.myhwclouds.com/std-conf/www.conf -O www.conf
 cd /etc
 wget https://yimian-setup.obs.myhwclouds.com/std-conf/my.cnf -O my.cnf
 systemctl restart mysqld
-yum -y install php72w-mysql
+##yum -y install php72w-mysql
 systemctl restart nginx
-systemctl restart php-fpm
+##systemctl restart php-fpm
 cd /home
 wget http://typecho.org/downloads/1.1-17.10.30-release.tar.gz
 tar -xvf 1.1-17.10.30-release.tar.gz
@@ -59,4 +68,4 @@ echo "*****************************************************************"
 echo 
 mysql -uroot
 echo 自动安装程序运行完成！！
-echo 请使用http://你的服务器ip 访问typecho
+echo 请使用浏览器输入 http://你的服务器ip 访问typecho
